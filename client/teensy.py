4056767ed4b554de8a345fee67dc01af4eda5aef
suricata3838 <summer970827@gmail.com>
2019-11-02 10:24:17 -0700

    add API call

--- client/teensy.py
+++ client/teensy.py
@@ -1,5 +1,6 @@
 from time import sleep
 import serial, random
+import requests
 from cryptography.hazmat.primitives.asymmetric.ed25519 import Ed25519PrivateKey, Ed25519PublicKey
 from cryptography.hazmat.primitives import serialization
 
@@ -25,3 +26,28 @@ public_key.verify(signature, b"my authenticated test message")
 loaded_public_key = public_key
 loaded_public_key.verify(signature, b"my authenticated test message")
 print("success!")
+
+
+# call python API
+# GET /Ledger/
+resp1 = requests.get('https://lens.perlin.net/ledger/')
+if resp.status_code != 200
+    # This means something went wrong.
+    raise ApiError('GET /tasks/ {}'.format(resp.status_code))
+for todo_item in resp.json():
+    print('GET /Ledger/')
+    print('{} {}'.format(todo_item['id'], todo_item['summary']))
+
+# POST /tx/send/
+## make payload
+
+post_json = {
+                "sender": "[hex-encoded sender ID, must be 32 bytes long]",
+                "tag": "[possible values: 0 = nop, 1 = transfer, 2 = contract, 3 = stake, 4 = batch",
+                "payload": "[hex-encoded payload, empty for nop]",
+                "signature": "[hex-encoded edwards25519 signature, which consists of private key, nonce, tag, and payload]"
+            }
+resp = requests.post('https://lens.perlin.net/tx/send/', json=post_json)
+if resp.status_code !=201:
+    raise ApiError('POST /tx/send/ {}'.format(resp.status_code)
+print('Send transaction: {}'.format(resp.json()["tx_id"]))
