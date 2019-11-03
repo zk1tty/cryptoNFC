726e4cb72352a21721df3aff9d2e633472c07c45
Eli Wenig <eli@neukind.jp>
2019-11-02 21:26:31 -0700

    updating get_public_key.py (minor)

--- client/get_public_key.py
+++ client/get_public_key.py
@@ -15,6 +15,7 @@ ser_byte = random.getrandbits(256)
 private_key = Ed25519PrivateKey.from_private_bytes(
     bytearray.fromhex('{:064x}'.format(ser_byte)))
 print("export MY_PRIVATE_KEY=", private_key.private_bytes(
-    serialization.Encoding.Raw, serialization.PrivateFormat.Raw))
+    serialization.Encoding.Raw, serialization.PrivateFormat.Raw,
+    serialization.NoEncryption()))
 print("export ACCOUNT_ID=", private_key.public_key().public_bytes(
     serialization.Encoding.Raw, serialization.PublicFormat.Raw))
