a51982a9c68bc4d2a3350d6724e05b099ceb7c7f
Eli Wenig <eli@neukind.jp>
2019-11-02 21:31:30 -0700

    fixing get_public_key.py

--- client/get_public_key.py
+++ client/get_public_key.py
@@ -1,3 +1,4 @@
+import binascii
 import random
 import serial
 import os
@@ -14,8 +15,10 @@ random.seed(UID)
 ser_byte = random.getrandbits(256)
 private_key = Ed25519PrivateKey.from_private_bytes(
     bytearray.fromhex('{:064x}'.format(ser_byte)))
-print("export MY_PRIVATE_KEY=", private_key.private_bytes(
-    serialization.Encoding.Raw, serialization.PrivateFormat.Raw,
-    serialization.NoEncryption()))
-print("export ACCOUNT_ID=", private_key.public_key().public_bytes(
-    serialization.Encoding.Raw, serialization.PublicFormat.Raw))
+print("export MY_PRIVATE_KEY={}".format(binascii.hexlify(
+    private_key.private_bytes(
+        serialization.Encoding.Raw, serialization.PrivateFormat.Raw,
+        serialization.NoEncryption())).decode("ascii")))
+print("export ACCOUNT_ID={}".format(binascii.hexlify(
+    private_key.public_key().public_bytes(
+        serialization.Encoding.Raw, serialization.PublicFormat.Raw)).decode("ascii")))
