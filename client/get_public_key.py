c56d132895a5d67f042fa3941b3d09223435b9b7
Eli Wenig <eli@neukind.jp>
2019-11-02 21:21:59 -0700

    updating get_public_key.py

--- client/get_public_key.py
+++ client/get_public_key.py
@@ -3,7 +3,6 @@ import serial
 
 ser = serial.Serial(os.getenv("SERIAL_DEVICE"), 9600)
 UID = ser.readline()
-# check
 print("-----------------------")
 print UID
 print("-----------------------")
@@ -12,5 +11,7 @@ random.seed(UID)
 ser_byte = random.getrandbits(256)
 private_key = Ed25519PrivateKey.from_private_bytes(
     bytearray.fromhex('{:064x}'.format(ser_byte)))
-print("puclic_key: ", public_key.public_bytes(
+print("export MY_PRIVATE_KEY=", private_key.private_bytes(
+    serialization.Encoding.Raw, serialization.PrivateFormat.Raw))
+print("export ACCOUNT_ID=", private_key.public_key().public_bytes(
     serialization.Encoding.Raw, serialization.PublicFormat.Raw))
